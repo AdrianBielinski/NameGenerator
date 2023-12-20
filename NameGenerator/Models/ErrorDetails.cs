71c56685d0d4c0a612886111fd2ea76ee20b35c5
AdrianBielinski <adidosek@gmail.com>
2023-12-20 19:12:53 +0100

    added more models

---
+++ NameGenerator/Models/ErrorDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace NameGenerator.API.Models
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ NameGenerator/Models/GenerationRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace NameGenerator.API.Models
+{
+    public class GenerationRequest
+    {
+        public int NumberOfRecords { get; set; }
+    }
+}
